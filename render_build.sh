0a68cfe415eef2d04bc1df1d6539ae60563cf339
Nicolas Brito Schrauth <165103017+Subatomicfat@users.noreply.github.com>
2025-11-24 23:20:49 -0300

    Update render_build.sh

--- render_build.sh
+++ render_build.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 # render_build.sh
 
-# Instala dependências
+# Instala todas as dependências do requirements.txt
 pip install -r requirements.txt
+
+# Instala explicitamente o dj-database-url como fallback
+pip install dj-database-url==1.3.0
 
 # Coleta arquivos estáticos
 python manage.py collectstatic --noinput
-
-# Executa migrações do banco de dados
-python manage.py migrate
