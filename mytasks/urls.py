2eb23e64d2e029152fe53d24b01a8b15e7830e07
Nicolas Brito Schrauth <165103017+Subatomicfat@users.noreply.github.com>
2025-11-24 22:44:44 -0300

    Update URL configuration to serve static files
    Added static file serving configuration for development.

--- mytasks/urls.py
+++ mytasks/urls.py
@@ -1,9 +1,21 @@
+"""
+URL configuration for mytasks project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+"""
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name='index.html'), name='home'),
     path('', include('tasks.urls')),
 ]
+
+# Configuração para servir arquivos estáticos em desenvolvimento
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
